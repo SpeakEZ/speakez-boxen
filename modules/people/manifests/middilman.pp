2caf4b7c75bd217868aa2054f19ba27463e862df
Geo <gmarshall@middil.com>
2013-02-21 09:38:14 -0700

    Added: middilman's personal manifest for boxen

---
+++ modules/people/manifests/middilman.pp
@@ -0,0 +1,10 @@
+class people::middilman {
+  include stdlib
+  include sysctl
+  include sparrow
+  include minecraft
+  include virtualbox
+  include sublime_text_2
+  include chrome
+  include vlc
+}
