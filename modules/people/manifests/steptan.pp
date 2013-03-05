cacccad4a99d71a7778f818d3b1abeb29e40f6e9
Stephen Felt <steptan@gmail.com>
2013-03-05 12:30:29 -0700

    Adding the wonderful magic of vim! and my pp file

---
+++ modules/people/manifests/steptan.pp
@@ -0,0 +1,39 @@
+class people::steptan {
+  include stdlib
+  include sysctl
+  include minecraft
+  include virtualbox
+  include vlc
+  include sublime_text_2
+  include chrome
+  include macvim
+#  include chrome::dev
+
+  # Include my projects
+  include projects::speakez
+  include projects::speakez-ios
+  include projects::canvas-lms
+  include projects::milbot
+
+  $home     = "/Users/${::luser}"
+  #$home     = "/Users/sfelt"
+  $code     = "${home}/code"
+  $dotfiles = "${code}/dotfiles"
+  $rubyversion = '2.0.0'
+
+  # Homebrew packages to install
+  package {
+    [
+      'bash-completion',
+      'htop-osx'
+    ]:
+  }
+
+  repository { $dotfiles:
+    source  => 'geothird/dotfiles'
+  }
+
+  exec { "set global ruby version":
+    command => "rbenv global ${rubyversion}"
+  }
+}
