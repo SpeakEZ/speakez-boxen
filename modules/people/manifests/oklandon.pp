acd37616a54aea05289951f883509ccf3baeba6e
Geo <gmarshall@middil.com>
2013-03-05 14:40:25 -0700

    Added: miles and landon github users.

---
+++ modules/people/manifests/oklandon.pp
@@ -0,0 +1,14 @@
+class people::oklandon {
+  include stdlib
+  include sysctl
+  include sublime_text_2
+  include chrome
+
+  $home     = "/Users/oklandon"
+  $sublime_home = "${home}/Library/Application\\ Support/Sublime\\ Text\\ 2/"
+
+  # Add RubyTest to Sublime text packages
+  repository { "$sublime_home/Packages/RubyTest":
+    source => 'maltize/sublime-text-2-ruby-tests'
+  }
+}
