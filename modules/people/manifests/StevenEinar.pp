aad03dd0c01ca39ab1742dc963d536d4dcf9f6f7
Adam Haymond <ahaymond@middil.com>
2013-05-14 15:46:17 -0600

    Added Steve's pp file

---
+++ modules/people/manifests/StevenEinar.pp
@@ -0,0 +1,26 @@
+# PP
+class people::StevenEinar {
+  include stdlib
+  include sysctl
+  include virtualbox
+  include sublime_text_2
+  include chrome
+
+  # include SpeakeEZ-Rails project
+  include projects::speakez-rails
+
+  $home     = "/Users/${::luser}"
+  $sublime_home = "${home}/Library/Application\sSupport/Sublime\sText\s2/"
+
+  package {
+    [
+      'bash-completion',
+      'htop-osx'
+    ]:
+  }
+
+  # Add RubyTest to Sublime text packages
+  repository { "$sublime_home/Packages/RubyTest":
+    source => 'maltize/sublime-text-2-ruby-tests'
+  }
+}
