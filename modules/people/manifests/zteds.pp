86d58dc380441e8b0b5a4dcdf44e7dcb5ffc22ce
Steve Hall <shall@solutionstream.com>
2013-11-06 16:53:56 -0700

    Adding a PP file for Ted Stockett. Copied from the StevenEinar.pp file.

---
+++ modules/people/manifests/zteds.pp
@@ -0,0 +1,21 @@
+class people::zteds {
+  include stdlib
+  include sysctl
+  include sublime_text_2
+  include chrome
+  #include elasticsearch
+
+  # include SpeakeEZ-Rails project
+  include projects::speakez-rails
+
+  # include Speakez flash project
+  include projects::speakez
+
+  $home     = "/Users/${::luser}"
+  $sublime_home = "${home}/Library/Application\\ Support/Sublime\\ Text\\ 2/"
+
+  # Add RubyTest to Sublime text packages
+  repository { "$sublime_home/Packages/RubyTest":
+    source => 'maltize/sublime-text-2-ruby-tests'
+  }
+}
